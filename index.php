0892b722c7b34cd5e3d2ce88341302b2ca9223e0
HardWar3 <32817150+HardWar3@users.noreply.github.com>
2019-05-13 10:28:57 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+
+function con(){
+
+    $host = 'localhost';
+    $user = 'root';
+    $pass = '';
+    $db = 'raumsystem';
+
+    $conn = new mysqli( $host, $user, $pass, $db ) or die( "connect failed: %s\n". $conn -> error );
+    
+    return $conn;
+
+}
+
+function closeCon( $conn ){
+
+    $conn->close();
+
+}
+
+$conn = con();
+
+$etagenQuery = $conn->query( "SELECT * FROM etagen" );
+$raeumeQuery = $conn->query( "SELECT * FROM raeume" );
+$plaetzeQuery = $conn->query( "SELECT * FROM plaetze" );
+
+$etagenResult = mysqli_fetch_all( $etagenQuery, MYSQLI_ASSOC );
+$raeumeResult = mysqli_fetch_all( $raeumeQuery, MYSQLI_ASSOC );
+$plaetzeResult = mysqli_fetch_all( $plaetzeQuery, MYSQLI_ASSOC );
+
+$result = array();
+
+$result[0] = json_encode( $etagenResult );
+$result[1] = json_encode( $raeumeResult );
+$result[2] = json_encode( $plaetzeResult );
+
+closeCon( $conn );
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Raum-Management-System</title>
+<link rel="stylesheet" type="text/css" href="css/style.css" />
+<script type="text/javascript" src="db/db.js" defer></script>
+<script type="text/javascript" src="js/mainJS.js" defer></script>
+</head>
+<body>
+<script type="text/javascript" >
+
+    var datenbankJsonPage = JSON.parse('<?php echo "{\"etagen\":" . $result[0] . ",\"raeume\":" . $result[1] . ",\"plaetze\":" . $result[2] . "}"?>');
+
+</script>
+</body>
+</html>
